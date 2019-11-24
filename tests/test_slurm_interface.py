3ee585bc6e780d5ba9e993da25e47437bc71da83
cristian ruiz <camilo1729@gmail.com>
2019-11-24 18:06:02 +0100

    Add tests for method get_jobs_state

--- tests/test_slurm_interface.py
+++ tests/test_slurm_interface.py
@@ -24,11 +24,14 @@ import pytest
 import mock
 import pytest_mock
 import ubench.scheduler_interfaces.slurm_interface as slurm_i
-
+import os
 
 class MockPopen(object):
     """ docstring """
 
+    def __init__(self,cmd):
+        self.cmd = cmd
+        
     # pylint: disable=R0201
     def wait(self):
         """ docstring """
@@ -37,7 +40,15 @@ class MockPopen(object):
     @property
     def stdout(self):
         """ docstring """
-        return ["node\tup\tinfinite\t16\tidle node[0006-0008,0020-0031]"]
+        if self.cmd == "sinfo":
+            return ["node\tup\tinfinite\t16\tidle node[0006-0008,0020-0031]"]
+        elif self.cmd == "squeue":
+            return ["   175757  RUNNING"]
+        elif self.cmd == "sacct":
+            return [" 26938.0     COMPLETED"," 26382.0     COMPLETED"]
+        else:
+            return [""]
+
 
 def test_emptylist():
     """ docstring """
@@ -50,7 +61,7 @@ def test_available_nodes(mocker):
     def mocksubpopen(args, shell, cwd, stdout=None, universal_newlines=False):
         """ docstring """
 
-        return MockPopen()
+        return MockPopen("sinfo")
 
     mock_popen = mocker.patch("ubench.scheduler_interfaces.slurm_interface.Popen",
                               side_effect=mocksubpopen)
@@ -60,3 +71,37 @@ def test_available_nodes(mocker):
     assert interface.get_available_nodes()
     assert len(node_list) == 3
     assert node_list[0] == "node[0006-0008,0020-0021]"
+
+def test_job_status(mocker):
+    """ docstring """
+    def mocksubpopen(args, shell, cwd, stdout=None, universal_newlines=False):
+        """ docstring """
+
+        return MockPopen("squeue")
+
+    mock_popen = mocker.patch("ubench.scheduler_interfaces.slurm_interface.Popen",
+                              side_effect=mocksubpopen)
+
+    interface = slurm_i.SlurmInterface()
+    assert interface.get_jobs_state(['111','222']) == {'175757' : 'RUNNING'}
+
+def test_job_status_2(pytestconfig,mocker):
+    """ docstring """
+    def mocksubpopen(args, shell, cwd, stdout=None, universal_newlines=False):
+        """ docstring """
+
+        return MockPopen("sacct")
+
+    mock_popen = mocker.patch("ubench.scheduler_interfaces.slurm_interface.Popen",
+                              side_effect=mocksubpopen)
+
+    interface = slurm_i.SlurmInterface()
+    jobs_info = interface.get_jobs_state(['111','222'])
+    exp_cmd ='sacct -n --jobs=111.0,222.0 --format=JobId,State'
+    repository_root = os.path.join(pytestconfig.rootdir.dirname,
+                                   pytestconfig.rootdir.basename)
+
+    mock_popen.assert_called_with(exp_cmd,cwd=repository_root,
+                                  shell=True, stdout=-1,universal_newlines=True)
+
+    assert jobs_info == {'26382': 'COMPLETED', '26938': 'COMPLETED'} 
